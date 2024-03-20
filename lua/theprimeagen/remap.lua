0cb3199461fe0c2c100a0f27c5d84118d9f5f727
Tony Andy Oehme <go98mub@mytum.de>
2024-03-20 02:05:09 +0100

    removed duplicate line

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -33,7 +33,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>w", [[:w!<CR>]])
-vim.keymap.set("n", "<leader>w", [[:w!<CR>]])
 vim.keymap.set("n", "<leader>q", [[:q!<CR>]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
