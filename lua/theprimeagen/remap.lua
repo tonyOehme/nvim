2c82e2c68b012c57230434726050e456b0768cee
Oehme, Tony Andy <tony-andy.oehme@adesso.de>
2024-04-22 01:45:26 +0200

    work on approuting

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -23,8 +23,9 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<C-a>", "ggVG")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
