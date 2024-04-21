f96430ed94c4668c0b468e7a7120aa127e56bfd0
Tony Andy Oehme <go98mub@myutm.de>
2024-04-21 23:12:40 +0200

    du stinkst

--- lua/theprimeagen/lazy/trouble.lua
+++ lua/theprimeagen/lazy/trouble.lua
@@ -11,13 +11,15 @@ return {
             end)
 
             vim.keymap.set("n", "[t", function()
-                require("trouble").next({skip_groups = true, jump = true});
+                require("trouble").next({ skip_groups = true, jump = true });
             end)
 
             vim.keymap.set("n", "]t", function()
-                require("trouble").previous({skip_groups = true, jump = true});
+                require("trouble").previous({ skip_groups = true, jump = true });
             end)
-
+            vim.keymap.set("n", "<leader>hf", function()
+                require("trouble").toggle("quickfix")
+            end)
         end
     },
 }
