990e912c8b1c0f302588e2ed8e8ad3068c42ec98
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-06-22 02:59:45 +0200

    automated push

--- lua/tony/set.lua
+++ lua/tony/set.lua
@@ -11,7 +11,6 @@ vim.opt.shiftwidth = 4
 vim.g.autoformat = true
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -26,3 +25,6 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
+vim.opt.autoread = true
+vim.opt.wrap = false
+vim.opt.wildmode = "longest,list"
